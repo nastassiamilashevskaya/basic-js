8dca6bce64ebe028a9af1a62e7af15d19b92ea07
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-03-11 01:10:33 +0300

    feat: add transform-array task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,44 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error;
+    }
+
+    if (!arr.length) {
+        return [];
+    }
+
+    let stack = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next': {
+                i++;
+                break;
+            }
+            case '--discard-prev': {
+                if (stack.length > 0) {
+                    stack.pop();
+                }
+                break;
+            }
+            case '--double-next': {
+                if (i < arr.length - 1) {
+                    stack.push(arr[i + 1]);
+                }
+                break;    
+            }
+            case '--double-prev': {
+                if (i > 0) {
+                    stack.push(arr[i - 1]);
+                }
+                break;
+            }
+            default: {
+                stack.push(arr[i]);
+                break;
+            }
+        }
+    }
+
+    return stack;
 };
