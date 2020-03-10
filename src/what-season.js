3c83693d265b4be0f02fc11f5bdd86d688cbe638
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-03-11 01:10:50 +0300

    feat: add what-season task solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,29 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let newDate = new Date;
+
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (newDate.prototype !== date.prototype || date.getUTCMonth() === undefined) {
+    throw new Error;
+  }        
+
+  let month = date.getMonth();
+
+  if (month === 11 || month < 2) {
+    return 'winter';
+  } else {
+    if (month >= 2 && month < 5) {
+      return 'spring';
+    } else {
+      if (month >= 5 && month < 8) {
+        return 'summer';
+      } else {
+        if (month >= 8 && month < 11) {
+          return 'autumn';
+        }
+      }
+    }
+  }
 };
