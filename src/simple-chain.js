b6f5299aae9d95800f2841361eb2d61c0ae8ae7e
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-03-21 01:04:24 +0300

    feat: add simple-chain task solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,35 @@
 const chainMaker = {
+  chain : [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push(' ');
+    } else {
+      this.chain.push(String(value));
+    }
+
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) || position > this.chain.length || position <= 0) {
+      this.chain = [];
+      throw new Error;
+    } else {
+      this.chain.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let finalChain = this.chain.map(x => '( ' + x + ' )').join('~~');
+    this.chain = [];
+    return finalChain;
   }
 };
 
