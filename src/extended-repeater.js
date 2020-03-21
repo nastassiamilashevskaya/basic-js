582458267462728c60daff932b1d3349a535e746
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-03-21 12:35:43 +0300

    feat: add extended-repeater task solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator } = options;
+    if (separator === undefined) {
+        separator = '+';
+    }
+    if (!additionSeparator) {
+        additionSeparator = '|';
+    }
+    if (addition === undefined) {
+        addition = '';
+    }
+    if (typeof addition !== 'string' || typeof str !== 'string') {
+        addition = String(addition);
+        str = String(str);
+    } 
+
+    const stringOfAddition = new Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+
+    return new Array(repeatTimes).fill(str + stringOfAddition).join(separator);
+
 };
   
