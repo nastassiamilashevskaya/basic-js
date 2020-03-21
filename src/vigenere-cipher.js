84795bba928e7171b474f33f1466ce1ce6af3450
nastassiamilashevskaya <52507944+nastassiamilashevskaya@users.noreply.github.com>
2020-03-21 22:55:21 +0300

    feat: add vigenere-cipher task solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,68 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor (typeOfMachine) {
+        typeOfMachine === false ? this.isReversed = true : this.isReversed = false;
+        this.alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    checkArguments(arg1, arg2) {
+        if (!arg1 || !arg2) {
+            throw new Error();
+        }
+    }
+
+    getResultString(array) {
+        if (this.isReversed) {
+            return array.reverse().join('');
+        } else {
+            return array.join('');
+        }
+    }
+
+    encrypt(message, key) {
+        this.checkArguments(message, key);
+
+        const messageArray = message.toUpperCase().split('');
+        const keyRepeatTimes = Math.ceil(message.length / key.length);
+        const keyArray = key.toUpperCase().repeat(keyRepeatTimes).split('');
+        let result = [];
+
+        messageArray.forEach(element => {
+            if (this.alphabets.indexOf(element) !== -1) {
+                result.push(this.alphabets[this.alphabets.indexOf(element) + this.alphabets.indexOf(keyArray.shift())]);
+            } else {
+                result.push(element);
+            }
+        });
+
+        return this.getResultString(result);
+    }
+
+    decrypt(encryptedMessage, key) {
+        this.checkArguments(encryptedMessage, key);
+
+        const messageArray = encryptedMessage.toUpperCase().split('');
+        const keyRepeatTimes = Math.ceil(encryptedMessage.length / key.length);
+        const keyArray = key.toUpperCase().repeat(keyRepeatTimes).split('');
+        let result = [];
+
+        messageArray.forEach(element => {
+            let indexOfCode = this.alphabets.indexOf(keyArray[0]);
+            let indexOfMessage = this.alphabets.indexOf(element);
+
+            if (indexOfMessage !== -1) {
+                keyArray.shift();
+                if (indexOfMessage <= indexOfCode) {
+                    result.push(this.alphabets[indexOfMessage - indexOfCode + 26]);
+                } else {
+                    result.push(this.alphabets[indexOfMessage - indexOfCode]);
+                }
+            } else {
+                result.push(element);
+            }
+        });
+
+        return this.getResultString(result);
     }
 }
 
